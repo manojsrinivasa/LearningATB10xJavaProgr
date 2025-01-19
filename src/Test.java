e09f9f3eab523cdc193be0a42eb21ab965e5b0d6
Manoj <ksmanoj789@gmail.com>
2025-01-19 12:26:52 +0530

    test

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+    int age = 16;
+    String result = (age <18) ? "Minor" : (age<65) ? "adult" : "Senior";
+        System.out.println(result);
+
+    }
+}
