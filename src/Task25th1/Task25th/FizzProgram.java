f6acf0474e9cf6470bcffbc16f2e91365f29c661
Manoj <ksmanoj789@gmail.com>
2025-01-26 13:29:09 +0530

    Task 25

---
+++ src/Task25th1/Task25th/FizzProgram.java
@@ -0,0 +1,26 @@
+package Task25th1.Task25th;
+
+public class FizzProgram {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i < 100; i++) {
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+                 }
+            else if (i % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            else System.out.println(i);
+
+        }
+
+
+    }
+
+
+}
